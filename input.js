3560de2c74a6a76c6c54d6cbc28da2f6baebef12
Spidey-N17A <nitinkumar.mat18@itbhu.ac.in>
2023-10-31 16:59:15 +0530

    searchfilter

--- input.js
+++ input.js
@@ -1,10 +1,25 @@
 
-const btn = document.getElementById('btn')
+// const btn = document.getElementById('btn')
 
-const name = document.getElementById('name')
+// const name = document.getElementById('name')
 
-btn.addEventListener("click",function searchone1(){
-    const searchurl = "extendedinput.html"
+// btn.addEventListener("click",function searchone1(){
+//     const searchurl = "extendedinput.html"
     
-    window.location.href = searchurl
+//     window.location.href = searchurl
+// })
+
+const  form = document.querySelector('form');
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const form_data = new FormData(form);
+    
+    for (item of form_data){
+        let str = item[0]+item[1];
+        localStorage.setItem(item[0],item[1]);
+    }
+
+    
+     window.location.href = "extendedinput.html"
+    
 })
