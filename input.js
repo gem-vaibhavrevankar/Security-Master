d13a0de53a3575c6c3c77ea26340975b7f4d9081
Vaibhav Revankar <vaibhav.revankar@geminisolutions.com>
2023-11-01 11:43:42 +0530

    update indentation

--- input.js
+++ input.js
@@ -1,25 +1,22 @@
-
 // const btn = document.getElementById('btn')
 
 // const name = document.getElementById('name')
 
 // btn.addEventListener("click",function searchone1(){
 //     const searchurl = "extendedinput.html"
-    
+
 //     window.location.href = searchurl
 // })
 
-const  form = document.querySelector('form');
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
-    const form_data = new FormData(form);
-    
-    for (item of form_data){
-        let str = item[0]+item[1];
-        localStorage.setItem(item[0],item[1]);
-    }
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form_data = new FormData(form);
 
-    
-     window.location.href = "extendedinput.html"
-    
-})
+  for (item of form_data) {
+    let str = item[0] + item[1];
+    localStorage.setItem(item[0], item[1]);
+  }
+
+  window.location.href = "extendedinput.html";
+});
