e6768ddce1e74a35430647656306e949fdad35ed
Vaibhav Revankar <vaibhav.revankar@geminisolutions.com>
2023-11-22 15:19:50 +0530

    sort fixes

--- sort.js
+++ sort.js
@@ -7,6 +7,9 @@ const headers = dataTable.querySelectorAll("thead th");
 // Object to store the current sort state for each column
 const sortState = {};
 
+// Object to store the number of times each column header is clicked
+const clickCount = {};
+
 // Function to sort the table data based on the selected column
 function sortTable(columnIndex) {
   const dataType = headers[columnIndex].getAttribute("data-type");
@@ -24,12 +27,31 @@ function sortTable(columnIndex) {
     }
   });
 
-  // Reverse the order if the column is already sorted in ascending order
-  if (sortState[columnIndex] === "asc") {
-    rows.reverse();
-    sortState[columnIndex] = "desc";
+  // Increment the click count for the current column
+  clickCount[columnIndex] = (clickCount[columnIndex] || 0) + 1;
+
+  // Set the default sort to be by ID every 3rd click
+  if (clickCount[columnIndex] % 3 === 0) {
+    // Sort by ID (assuming ID is a numeric column)
+    rows.sort((a, b) => {
+      const aId = parseFloat(a.cells[0].textContent.trim());
+      const bId = parseFloat(b.cells[0].textContent.trim());
+      return aId - bId;
+    });
+
+    // Reset click count
+    clickCount[columnIndex] = 0;
+
+    // Set the sort state to ascending for the ID column
+    sortState[0] = "asc";
   } else {
-    sortState[columnIndex] = "asc";
+    // Reverse the order if the column is already sorted in ascending order
+    if (sortState[columnIndex] === "asc") {
+      rows.reverse();
+      sortState[columnIndex] = "desc";
+    } else {
+      sortState[columnIndex] = "asc";
+    }
   }
 
   // Remove existing rows from the table
